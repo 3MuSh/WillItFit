c90e6b62e363b9f4279ced410a55187e6f18fafe
xarines <wmarch112@gmail.com>
2016-03-13 14:52:33 +0100

    add comment

--- app/src/main/java/com/example/android/willitfit/MainActivity.java
+++ app/src/main/java/com/example/android/willitfit/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.android.willitfit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/**
+ * add javadoc comment
+  */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
