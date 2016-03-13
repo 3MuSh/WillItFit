8e765ceafc78c1e780d5dca9165f693e802e7770
pirzmak <pirzmul@interia.eu>
2016-03-13 16:41:24 +0100

    spirites class2

---
+++ app/src/main/java/com/example/android/willitfit/Spirites.java
@@ -0,0 +1,21 @@
+package com.example.android.willitfit;
+
+import android.media.Image;
+
+/**
+ * Created by Ja on 2016-03-13
+ */
+public class Spirites {
+    Image image;
+    int x;
+    int y;
+
+    Spirites(int a,int b,String img){
+        init_Spirites(a,b,img);
+    }
+
+    private void init_Spirites(int a,int b,String img){
+        x=a;
+        y=b;
+    }
+}
