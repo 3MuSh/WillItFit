4d14a92502c768e2b4a58afc260af393a5796052
Zwirek009 <maciek.wiraszka@gmail.com>
2016-03-19 00:05:01 +0100

    add icons, change mainactivity

--- app/src/main/java/com/example/android/willitfit/MainActivity.java
+++ app/src/main/java/com/example/android/willitfit/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.willitfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 /**
@@ -13,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_main);*/
+
         setContentView(R.layout.activity_main);
     }
 }
