e77a0652b70ff6cf1101c401a0714690db06c171
Zwirek009 <maciek.wiraszka@gmail.com>
2016-03-19 17:18:55 +0100

    add buttonshape, add closeApp()

--- app/src/main/java/com/example/android/willitfit/MainActivity.java
+++ app/src/main/java/com/example/android/willitfit/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.willitfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -24,5 +25,10 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
 
         setContentView(R.layout.activity_main);
+    }
+
+    public void closeApp(View view)
+    {
+        System.exit(0);
     }
 }
